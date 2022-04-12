5476426d5e27605cb801aeb7aeaba2aa446c1856
Austin A <austinandrews89@gmail.com>
2022-04-11 23:08:59 -0600

    Started building Work cards

--- src/Components/WorkModal.js
+++ src/Components/WorkModal.js
@@ -1,10 +1,85 @@
-import { Container, Col, Nav, Modal, Row, Image } from "react-bootstrap";
+import { Container, Col, Nav, Modal, Row, Image, Card } from "react-bootstrap";
 import { useState } from "react";
 
 function WorkModal(){
+    const [showHome, setShowHome] = useState(false);
+
+    const handleShow = () => setShowHome(true);
+    const handleHide = () => setShowHome(false);
+
     return(
         <>
-            <Nav.Link className="App-link">Work</Nav.Link>
+            <Nav.Link className="App-link" onClick={handleShow}>Work</Nav.Link>
+            <Modal size = "xl" centered show={showHome} onHide = {handleHide}>
+
+            <Modal.Body className="App-modal">
+                <Container>
+                    <Row>
+                        <Col>
+                            <Card>
+                                <Card.Img variant="top" src="AustinAProfile.jpg" />
+                                <Card.Title>Project</Card.Title>
+                                <Card.Text>HEY LOOK AT ME I'M A CARD PLEASE GIVE ME BETTER CONTENT</Card.Text>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                            <Card.Img variant="top" src="AustinAProfile.jpg" />
+                            <Card.Title>Project</Card.Title>
+                            <Card.Text>HEY LOOK AT ME I'M A CARD PLEASE GIVE ME BETTER CONTENT</Card.Text>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Card>
+                            <Card.Img variant="top" src="AustinAProfile.jpg" />
+                            <Card.Title>Project</Card.Title>
+                                <Card.Text>HEY LOOK AT ME I'M A CARD PLEASE GIVE ME BETTER CONTENT</Card.Text>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                            <Card.Img variant="top" src="AustinAProfile.jpg" />
+                            <Card.Title>Project</Card.Title>
+                                <Card.Text>HEY LOOK AT ME I'M A CARD PLEASE GIVE ME BETTER CONTENT</Card.Text>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                            <Card.Img variant="top" src="AustinAProfile.jpg" />
+                            <Card.Title>Project</Card.Title>
+                                <Card.Text>HEY LOOK AT ME I'M A CARD PLEASE GIVE ME BETTER CONTENT</Card.Text>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Card>
+                            <Card.Img variant="top" src="AustinAProfile.jpg" />
+                            <Card.Title>Project</Card.Title>
+                                <Card.Text>HEY LOOK AT ME I'M A CARD PLEASE GIVE ME BETTER CONTENT</Card.Text>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                            <Card.Img variant="top" src="AustinAProfile.jpg" />
+                            <Card.Title>Project</Card.Title>
+                                <Card.Text>HEY LOOK AT ME I'M A CARD PLEASE GIVE ME BETTER CONTENT</Card.Text>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card>
+                            <Card.Img variant="top" src="AustinAProfile.jpg" />
+                            <Card.Title>Project</Card.Title>
+                                <Card.Text>HEY LOOK AT ME I'M A CARD PLEASE GIVE ME BETTER CONTENT</Card.Text>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </Modal.Body>
+
+        </Modal>
         </>
     )
 }
